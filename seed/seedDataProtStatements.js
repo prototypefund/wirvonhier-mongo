6858afcbc796fc83a77c778627fabe0e3ddc8f47
MarcRoemmelt <mail@m-roemmelt.com>
2020-08-17 17:14:04 +0200

    fix: typo

--- seed/seedDataProtStatements.js
+++ seed/seedDataProtStatements.js
@@ -1,5 +1,5 @@
 const res = [
   "Inserting DataProtStatements",
-  db.dataprotstatements.insert({ version: '1.0.0', type: 'dummy', langauge: 'de', text: 'dummy', html: '<h1>Dummy</h1>', markdown: '#dummy' }),
+  db.dataprotstatements.insert({ version: '1.0.0', type: 'dummy', language: 'de', text: 'dummy', html: '<h1>Dummy</h1>', markdown: '#dummy' }),
 ]
 printjson(res);
